762b242ee0ddc67eb1718e105eca3591b15b1c6a
Kjiel Carlson <kjielcarlson@Kjiels-MacBook-Pro.local>
2017-01-28 17:17:11 -0800

    first commit

---
+++ dog.js
@@ -0,0 +1,11 @@
+console.log("I'm Milo!")
+
+var d = {
+    name: "Milo",
+    type: "Awesome",
+    apparel: "Smoking Jacket"
+}
+
+if (d && d.apparel){
+    console.log(d.name + " looks good in his " + d.apparel)
+}
